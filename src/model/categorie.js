f1f22b448be35cddda1bdb5e78f86cd2fa83071f
MarianaDias <marianadias14b@hotmail.com>
2024-11-21 10:48:58 +0100

    wip

---
+++ src/model/categorie.js
@@ -0,0 +1,14 @@
+class Categorie extends Produit {
+    #nom
+    constructor(nom){
+        this.#nom
+    }
+
+    get nom(){
+        return this.#nom
+    }
+
+    set nom(nom){
+        nom = this.#nom
+    }
+}
---
+++ src/model/produit.js
@@ -0,0 +1,28 @@
+class Produit {
+    #nom
+    #prix
+
+
+    constructor(nom, prix) {
+        this.#nom = nom
+        this.#prix = prix
+    }
+
+    get nom() {
+        return this.#nom
+    }
+    get prix() {
+        return this.#prix
+    }
+
+    set nom(nom){
+        nom = this.#nom
+    }
+    set prix(prix){
+        prix = this.#prix
+    }
+
+    
+}
+
+module.exports = Produit
