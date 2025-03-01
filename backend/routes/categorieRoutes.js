d8b028d03b8965a3bb107fa8b144cdaf9dfa649a
simon-kercode <sim.malry@gmail.com>
2025-03-01 14:02:52 +0100

    route fix

--- backend/routes/categorieRoutes.js
+++ backend/routes/categorieRoutes.js
@@ -3,8 +3,8 @@ const CategorieController = require('../controllers/CategorieController');
 const {authMiddleware} = require("../middlewares/authMiddleware")
 const router = express.Router();
 
-router.get('/', authMiddleware, CategorieController.getAll);
-router.get('/:id', authMiddleware, CategorieController.getById)
+router.get('/', CategorieController.getAll);
+router.get('/:id', CategorieController.getById)
 router.post('/', authMiddleware, CategorieController.create);
 router.put('/:id', authMiddleware, CategorieController.update);
 router.delete('/:id', authMiddleware, CategorieController.delete);
--- backend/routes/produitRoutes.js
+++ backend/routes/produitRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const ProduitController = require('../controllers/ProduitController');
 const {authMiddleware} = require("../middlewares/authMiddleware")
 
-router.get('/', authMiddleware, ProduitController.getAll);
-router.get('/categories', authMiddleware, ProduitController.getAllWithCategories);
-router.get('/:id', authMiddleware, ProduitController.getById);
+router.get('/', ProduitController.getAll);
+router.get('/categories', ProduitController.getAllWithCategories);
+router.get('/:id', ProduitController.getById);
 router.post('/', authMiddleware, ProduitController.create);
 router.put('/:id', authMiddleware, ProduitController.update);
 router.delete('/:id', authMiddleware, ProduitController.delete);
