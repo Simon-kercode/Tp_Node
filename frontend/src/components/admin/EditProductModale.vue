dcaa8f1734bff330acb16f106536de6b4887287d
simon-kercode <sim.malry@gmail.com>
2025-03-13 23:11:41 +0100

    illustration in modale

--- frontend/src/components/admin/EditProductModale.vue
+++ frontend/src/components/admin/EditProductModale.vue
@@ -9,6 +9,7 @@
                             <v-text-field
                                 v-model="product.produit_nom"
                                 label="Nom"
+                                clearable
                             ></v-text-field>                            
                         </v-col>
                         <v-col cols="12" md="4">
@@ -35,6 +36,15 @@
                     chips
                 >
                 </v-select>
+                <div class="d-flex">
+                    <v-file-input
+                        label="Illustration"
+                        clearable
+                        @change="handleFileChange"
+                        :model-value="fileName"
+                    ></v-file-input>
+                    <v-img v-if="previewImage" :src="previewImage" height="100px"></v-img>                    
+                </div>
             </v-card-text>
             <v-card-actions>
                 <v-btn @click="updateProduct" color="success" variant="flat">Sauvegarder</v-btn>
@@ -51,14 +61,33 @@
 
     const productStore = useProductStore();
     const {editProductModaleState} = storeToRefs(productStore);
+
     const product = ref({
         ...productStore.productToEdit, 
         prix: parseFloat(productStore.productToEdit.prix)
     });
-
     const initialProduct = ref({...product});
 
     const categories = ref(productStore.__ListCategories);
+
+    const newFile = ref(null);
+    const previewImage = ref(`/uploads/productsImages/${product.value.illustration}`);
+
+    const fileName = computed(() => {
+        return newFile.value ? newFile.value.name : product.value.illustration;
+    });
+
+    // Gère le changement de fichier dans l'input file d'illustration. Affiche le nom correct et la preview.
+    function handleFileChange(event) {
+        const file = event.target.files[0]; // Récupére le fichier sélectionné
+        if (file) {
+            newFile.value = file; // Stocke le fichier pour l'envoi
+            previewImage.value = URL.createObjectURL(file); // Génére un aperçu temporaire
+        } else {
+            newFile.value = null;
+            previewImage.value = `/uploads/productsImages/${product.value.illustration}`; // Réaffiche l'image d'origine
+        }
+    }
 
     async function updateProduct() {
         const productData = {
