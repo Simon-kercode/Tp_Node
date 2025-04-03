7393822569c68c0aa6dd765d4ae5a05bae4d73d0
simon-kercode <sim.malry@gmail.com>
2025-04-03 22:53:29 +0200

    profile responsiv fix

--- frontend/src/components/profil/UserInfos.vue
+++ frontend/src/components/profil/UserInfos.vue
@@ -1,5 +1,5 @@
 <template>
-<v-card variant="text" min-width="500">
+<v-card variant="text" min-width="50%">
   <v-card-title class="text-h6 font-weight-bold">Bienvenue  {{ user.firstname }}</v-card-title>
   
   <v-card-text>
@@ -23,10 +23,10 @@
       <v-list-item>
         <v-row class="d-flex align-center">
           <v-col cols="4" class="font-weight-bold">Mot de passe :</v-col>
-          <v-col cols="5">********</v-col>
-          <v-col cols="3" class="text-right">
-            <v-btn class="custom-btn" variant="text" density="compact" @click="editPassword">Modifier</v-btn>
-          </v-col>
+          <v-col cols="8" md="3">********</v-col>
+          <v-col cols="8" md="5" class="text-right">
+            <v-btn min-width="120" class="custom-btn" variant="text" density="compact" @click="editPassword">Modifier</v-btn>
+          </v-col>            
         </v-row>
       </v-list-item>
     </v-list>
@@ -38,7 +38,6 @@
     import { ref } from 'vue';
     import { useAuthStore } from '../../stores/authStore';
     import { useUserStore } from '../../stores/userStore';
-    import { passwordRule } from '../../utils/formRules';
 
     const authStore = useAuthStore();
     const userStore = useUserStore();
